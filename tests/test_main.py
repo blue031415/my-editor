b9050de512a3f23eed2cbb4805014b1a3c34a406
blue031415 <blue031415@gmail.com>
2024-12-30 14:06:34 +0900

    fix : 一行の文字数の規則の競合を解消(flake8とblack)

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,15 @@
 import sys
 from io import StringIO
+import os
+
+# srcディレクトリをモジュール検索パスに追加
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src"))
+)
 
 
 def test_main_output():
-    from src.main import main
+    from main import main
 
     captured_output = StringIO()
     sys.stdout = captured_output
