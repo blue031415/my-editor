e2e6bda81b7441bbe6bbafda60ffef9320dfb54f
blue031415 <blue031415@gmail.com>
2024-12-30 13:37:11 +0900

    fix: ディレクトリ構成

--- python_flet.bat
+++ python_flet.bat
@@ -1,7 +1,17 @@
-mkdir python_flet
-cd python_flet
-python -m venv env
+@echo off
 
-cd env
-Scripts\python -m pip install --upgrade pip
-Scripts\pip install flet black flake8
+REM 仮想環境のディレクトリが存在するか確認
+IF EXIST .venv (
+    REM 存在する場合は削除
+    rmdir /s /q .venv
+)
+
+REM 新しい仮想環境を作成
+python -m venv .venv
+
+REM 仮想環境をアクティベート
+call .venv\Scripts\activate
+
+REM 必要なパッケージをインストール
+.\.venv\Scripts\python.exe -m pip install --upgrade pip
+.\.venv\Scripts\pip.exe install -r requirements.txt
